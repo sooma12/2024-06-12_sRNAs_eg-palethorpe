d869e385d7fc3a856d8bc7ae27a605fcc5ba376e
soom12 <soo.m@northeastern.edu>
2024-06-12 14:10:04 -0400

    fixed samtools eg script

--- scripts/4A_sbatch_samtools_to_sorted_bam_eg.sh
+++ scripts/4A_sbatch_samtools_to_sorted_bam_eg.sh
@@ -20,11 +20,11 @@ module load samtools/1.19.2
 
 source ./config.cfg
 
-echo "Looking for .sam files and outputting sorted bams in $MAPPED_DIR_eg"
+echo "Looking for .sam files and outputting sorted bams in $MAPPED_DIR_eg_VerySens"
 
 # Get array of sam files
 # shellcheck disable=SC2207
-sams_array=($(ls -d ${MAPPED_DIR_eg}/*.sam))
+sams_array=($(ls -d ${MAPPED_DIR_eg_VerySens}/*.sam))
 
 # Get specific file for this array task
 current_file=${sams_array[$SLURM_ARRAY_TASK_ID-1]}
@@ -33,15 +33,15 @@ current_name=$(basename "$current_file")
 current_name_no_ext="${current_name%.*}"
 
 echo "converting .sam to .bam"
-samtools view -bS "${current_file}" > ${MAPPED_DIR_eg}/${current_name_no_ext}.bam
+samtools view -bS "${current_file}" > ${MAPPED_DIR_eg_VerySens}/${current_name_no_ext}.bam
 echo "sorting bam file"
-samtools sort ${MAPPED_DIR_eg}/${current_name_no_ext}.bam -o "${MAPPED_DIR_eg}/${current_name_no_ext}"_sorted.bam
+samtools sort ${MAPPED_DIR_eg_VerySens}/${current_name_no_ext}.bam -o "${MAPPED_DIR_eg_VerySens}/${current_name_no_ext}"_sorted.bam
 echo "analyzing alignment statistics"
-samtools stats "${MAPPED_DIR_eg}/${current_name_no_ext}"_sorted.bam > "${MAPPED_DIR_eg}/${current_name_no_ext}"_sorted.bam.stats
+samtools stats "${MAPPED_DIR_eg_VerySens}/${current_name_no_ext}"_sorted.bam > "${MAPPED_DIR_eg_VerySens}/${current_name_no_ext}"_sorted.bam.stats
 
-echo "cleaning up: moving .sam and unsorted .bam files to $MAPPED_DIR_eg/intermediate_files"
-mkdir -p $MAPPED_DIR_eg/intermediate_files
+echo "cleaning up: moving .sam and unsorted .bam files to $MAPPED_DIR_eg_VerySens/intermediate_files"
+mkdir -p $MAPPED_DIR_eg_VerySens/intermediate_files
 echo "moving ${current_file}"
-mv ${current_file} $MAPPED_DIR_eg/intermediate_files
+mv ${current_file} $MAPPED_DIR_eg_VerySens/intermediate_files
 echo "moving ${current_file}${current_name_no_ext}.bam"
-mv $MAPPED_DIR_eg/"${current_name_no_ext}".bam $MAPPED_DIR_eg/intermediate_files
+mv $MAPPED_DIR_eg_VerySens/"${current_name_no_ext}".bam $MAPPED_DIR_eg_VerySens/intermediate_files
